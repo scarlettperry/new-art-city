b9f206cc7ccfb29ffc2c26f068aeaabc301334ea
scarlettperry <si.perry15@gmail.com>
2018-10-24 17:40:58 -0400

    added save functionality from the info card

--- src/components/MapEventCard.js
+++ src/components/MapEventCard.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Accordion, Icon } from 'semantic-ui-react'
+import moment from 'moment'
+
+import { postUserEvents } from '../actions/railsAPI'
+
 
 class MapEventCard extends Component {
 
+  state = {
+    toggled: false,
+    activeIndex: 0
+  }
+
+  handleToggle = () => {
+    this.setState((prevState)=>{
+      return {toggled: !prevState.toggled}
+    })
+    this.props.postUserEvents(this.props.event)
+  }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
   render(){
-    console.log(this.props.event)
+
+    const { activeIndex } = this.state
+
     return(
       <div className="ui items">
         <div className='item'>
@@ -12,9 +40,28 @@ class MapEventCard extends Component {
             />
           </div>
           <div className='content'>
-            <div className='meta'>@{this.props.event.Venue.Name["_text"]}</div>
-            <div className='description'>{this.props.event.Name["_text"]}</div>
-            <button>Save to Profile</button>
+            <button onClick={this.handleToggle} className='ui small icon button' role='button'>
+              <i aria-hidden='true' className='red heart icon' />
+              {!this.state.toggled? <span> Save</span> : <span> Unsave</span> }
+            </button><br/><br/>
+            <div className='meta'>
+              {this.props.event.DateEnd["_text"] === "0000-00-00" ? "Ongoing" : `Through ${moment(this.props.event.DateEnd["_text"]).format("MMM D, YYYY")}`} @ {this.props.event.Venue.Name["_text"]}
+            </div>
+            <div className='description'><strong>{this.props.event.Name["_text"]}</strong>
+          </div>
+            <Accordion>
+              <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
+                <Icon name='dropdown' />
+                Venue details
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 2}>
+                <p>
+                  <strong>Address:</strong> {this.props.event.Venue.Address["_text"]}<br/>
+                  <strong>Hours:</strong> {moment(this.props.event.Venue.OpeningHour["_text"],'HH:mm:ss' ).format('h:mm A')} - {moment(this.props.event.Venue.ClosingHour["_text"], 'HH:mm:ss').format('h:mm A')}<br/>
+                  <strong>Phone Number:</strong> {this.props.event.Venue.Phone["_text"]}
+                </p>
+              </Accordion.Content>
+            </Accordion>
           </div>
         </div>
       </div>
@@ -22,4 +69,4 @@ class MapEventCard extends Component {
   }
 }
 
-export default MapEventCard
+export default connect(null, { postUserEvents })(MapEventCard)
