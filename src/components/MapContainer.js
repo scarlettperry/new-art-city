7c3a2c7301c8d6fa098f720e487d968b9f32de2c
scarlettperry <si.perry15@gmail.com>
2018-10-24 16:26:58 -0400

    added api key

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -4,10 +4,11 @@ import Map from "../components/Map";
 export default class MapContainer extends Component {
 
 	render() {
+		const API_KEY = process.env.REACT_APP_GOOGLE_API
 		return (
 			<div className="map-container">
 				<Map
-					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo&v=3.exp&libraries=geometry,drawing,places`}
+					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
 					loadingElement={<div style={{ height: `100%` }} />}
 					containerElement={<div style={{ height: `600px`, width: `450px` }} />}
 					mapElement={<div style={{ height: `100%` }} />}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// require('dotenv').config()
+
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
